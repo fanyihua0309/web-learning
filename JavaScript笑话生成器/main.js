ea908fd146cae07d7085cd07ebb57d3c0542020a
“fanyihua0309” <nuaaccstfyh@163.com>
2021-04-01 19:25:30 +0800

    修改错误代码，可以正常实现

--- JavaScript笑话生成器/main.js
+++ JavaScript笑话生成器/main.js
@@ -6,7 +6,7 @@ function randomValueFromArray(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-let storeText = '今天气温 34 摄氏度，:inserta:出去遛弯。当走到:insertb:门前时，突然就:insertc:。人们都惊呆了，李雷全程目睹但并没有慌，因为:inserta:是一个 130 公斤的胖子，天气又辣么热。';
+let storyText = '今天气温 34 摄氏度，:inserta:出去遛弯。当走到:insertb:门前时，突然就:insertc:。人们都惊呆了，李雷全程目睹但并没有慌，因为:inserta:是一个 130 公斤的胖子，天气又辣么热。';
 let insertX = ['怪兽威利', '大老爹', '圣诞老人'];
 let insertY = ['肯德基', '迪士尼乐园', '白宫'];
 let insertZ = ['自燃了', '在人行道化成了一坨泥', '变成一条鼻涕虫爬走了'];
